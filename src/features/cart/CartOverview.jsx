1375fbf9e0e35caee175498130837bf51e8272fa
ReiKoka <reikoka1@outlook.com>
2024-05-16 02:42:33 +0200

    first commit

---
+++ src/features/cart/CartOverview.jsx
@@ -0,0 +1,28 @@
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { getTotalCartPrice, getTotalCartQuantity } from "./cartSlice";
+import { formatCurrency } from "./../../utils/helpers";
+
+function CartOverview() {
+  const totalCartQuantity = useSelector(getTotalCartQuantity);
+  const totalCartPrice = useSelector(getTotalCartPrice);
+
+  if (!totalCartQuantity) return null;
+
+  return (
+    <div className=" flex items-center justify-between bg-stone-800 px-4 py-4 text-sm uppercase text-stone-200 sm:px-6 md:text-base">
+      <p className="space-x-4 font-semibold uppercase text-stone-300 sm:space-x-6">
+        <span>{totalCartQuantity} pizzas</span>
+        <span>{formatCurrency(totalCartPrice)}</span>
+      </p>
+      <Link
+        className="border-0 outline-0 bg-inherit hover:text-yellow-500 focus:text-yellow-500 focus:ring focus:ring-offset-2 active:scale-95"
+        to="/cart"
+      >
+        Open cart &rarr;
+      </Link>
+    </div>
+  );
+}
+
+export default CartOverview;
